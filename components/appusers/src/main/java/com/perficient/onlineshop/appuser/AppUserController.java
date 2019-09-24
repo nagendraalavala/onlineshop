deeb31b5f89c389209156539680d5f86e4997f50
Kaitlyn Spehr <kaitlyn.spehr@perficient.com>
2019-09-24 11:19:22 -0400

    implementing search

--- components/appusers/src/main/java/com/perficient/onlineshop/appuser/AppUserController.java
+++ components/appusers/src/main/java/com/perficient/onlineshop/appuser/AppUserController.java
@@ -3,6 +3,7 @@ package com.perficient.onlineshop.appuser;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -13,10 +14,17 @@ public class AppUserController {
     public AppUserController(AppUserRepo appUserRepo) {
         this.appUserRepo = appUserRepo;
     }
+
     @GetMapping("/appusers")
     public String allAppUsers(Map<String,Object> model) {
         model.put("appusers", appUserRepo.findAll());
         return "appusers";
+    }
+
+    @GetMapping("/appuser")
+    public String view(@RequestParam(name="id") String id, Map<String,Object> model) {
+        model.put("appuser", appUserRepo.findById(Long.parseLong(id)).get());
+        return "appuser";
     }
 }
 
